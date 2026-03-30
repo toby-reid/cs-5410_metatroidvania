e3460f43b8b8c81cdff8a35266c182e954ca40cf
Tyler Allen <tylerjallen02@gmail.com>
2026-03-30 08:47:11 -0600

    Connect main menu to main game

--- UI/MainMenu/MainMenu.cs
+++ UI/MainMenu/MainMenu.cs
@@ -3,13 +3,17 @@ using Global;
 
 public partial class MainMenu : Control
 {
+    [ExportGroup("Buttons")]
     [Export] private Button _newGameButton;
     [Export] private Button _continueButton;
     [Export] private Button _exitButton;
-    
+
+    [ExportGroup("Scenes")]
+    [Export(PropertyHint.File, "*.tscn")] private string _mainGamePath;
+
     public override void _Ready()
     {
-        _newGameButton.Pressed += () => GD.Print("TODO: connect to new game");
+        _newGameButton.Pressed += () => SceneChanger.Instance.ChangeScene(_mainGamePath);
         _continueButton.Pressed += () => GD.Print("TODO: connect to continue game");
         _exitButton.Pressed += () => SceneChanger.Instance.ExitGame();
     }

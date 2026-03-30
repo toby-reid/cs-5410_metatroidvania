2107b4f922f60b12f29b416a36488005514e9707
Tyler Allen <tylerjallen02@gmail.com>
2026-03-30 00:34:31 -0600

    Implement room manager

---
+++ Levels/RoomManager/RoomManager.cs
@@ -0,0 +1,36 @@
+using Godot;
+
+public partial class RoomManager : Node
+{
+    [Export] public Node2D _roomContainer;
+    [Export] public RoomChangeBus _roomChangeBus;
+    
+    [ExportGroup("Start Room")]
+    [Export(PropertyHint.File, "*.tscn")] private string startRoom;
+    [Export] private int startDoor;
+
+    public override void _Ready()
+    {
+        ChangeRoom(startRoom, startDoor);
+        _roomChangeBus.OnRoomChangeRequest += ChangeRoom;
+    }
+
+    public override void _ExitTree()
+    {
+        _roomChangeBus.OnRoomChangeRequest -= ChangeRoom;
+    }
+
+    private void ChangeRoom(string scene, int doorId)
+    {
+        // there should only be one child, but this is safer
+        foreach (Node room in _roomContainer.GetChildren())
+        {
+            room.QueueFree();
+        }
+
+        Node newRoom = ResourceLoader.Load<PackedScene>(scene).Instantiate();
+        _roomContainer.AddChild(newRoom);
+
+        // TODO put the player at the room
+    }
+}
